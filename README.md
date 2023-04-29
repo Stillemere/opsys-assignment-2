6c7cb67ac75d3a1b9a32cdfcd207d42b8649e891
Stillemere <hellotoad3@gmail.com>
2023-04-28 23:48:04 -0400

    Near complete

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # opsys-assignment-2
 The second programming assignment of the Operating Systems course.
+
+This program demonstrates the banker's algorithm given the data of a system.
+It determines whether the system is safe (i.e. there will not be deadlock).
+
+Aspects of the program:
+
+- The allocation matrix shows the resources allocated to each process.
+
+- The maximum resource (max_res) matrix represents the maximum resources each process can request.
+
+- The availability vector represents the available resources of the system.
+
+- The need matrix is calculated from the max matrix and allocation matrix (N = M - A).
+
+Checking whether the system is in a safe state:
+- Find a process i such that need[i][j] <= work[j] and finished[i] is false.
+- For each such process, add the corresponding entry in the allocation matrix to the work vector.
+- Set each element of the finished vector to true for each such process.
+- If all elements in the finished vector are true, the system is in a safe state.
--- bankers_alg.c
+++ bankers_alg.c
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "sysdata.h"
 
+int safe_sequence[5] = {-1,-1,-1,-1,-1};
+
 int main(){
    // note: still need to retrieve data for allocation matrix, etc
    // and number of processes, res types
@@ -17,6 +19,12 @@ int main(){
    if(isSafe(availability)){
        std::cout << "The system is in a safe state" << std::endl;
        std::cout << "Safe sequence: " << std::endl;
+       for(int i = 0; i < 5; ++i){
+           if(safe_sequence[i] == -1) continue; // unfilled slot
+           std::cout << 'P' << safe_sequence[i];
+           if(i<4) std::cout << ", ";
+           else std::cout << '\n';
+       }
    }
    else std::cout << "The system is not in a safe state." << std::endl;
 }
@@ -27,41 +35,30 @@ bool isSafe(const std::vector<int>& available_t){
    // initialize tmp array (work = available)
    std::cout << "work[]: ";
    for(int i = 0; i < res_types; ++i){
-      work[i] = available_t[i]; // debug note: this code's good
+      work[i] = available_t[i];
       std::cout << '[' << work[i] << ']';
    }
    std::cout << '\n';
    // find a process i for which the process is avalable
    // if so, set finished[i] to true and assign the input from the
    // availablility vector into the allocation matrix
+   int k = 0; // safe sequence index
    for(int i = 0; i < num_processes; ++i){
        for(int j = 0; j < res_types; ++j){
-           //std::cout << i << ',' << j << " | ";
-           if(need[i][j] <= work[j]){
-               //std::cout << need[i][j] << " <= " << work[i] << " | ";
-	       //std::cout << work[j] << " | ";
+           if(need[i][j] <= work[j] && finished[i] == 0){
                work[j] += allocation[i][j];
-               //std::cout << work[j] << std::endl;
                finished[i] = 1;
+               safe_sequence[k] = i;
            }
-           //std::cout << '\n';
-           //else { std::cout << need[i][j] << " !<= " << work[i] << " | "; }
-           /*
-           if(finished[i] == 0 && need[i][j] <= available_t[i]){
-               allocation[i][j] = available_t[i];
-               finished[i] == 1;
-               safe_count++;
-           }
-           */
        }
-       //std::cout << '\n';
+       ++k;
    }
    std::cout << "Finished vector: \n";
    for(int i = 0; i < num_processes; ++i) std::cout << '[' << finished[i] << ']';
    std::cout << '\n';
 
    int safe_count;
-   for(int i = 0; i < finished.size(); i++) if(finished[i] == 1) ++safe_count;
-   if(safe_count == num_processes) return 1;
+   for(int i = 0; i < finished.size(); i++) if(finished[i]) ++safe_count;
+   if(safe_count == num_processes) return 1; // all elements in the finished vector are true therefore it is in a safe state
    return 0;
 }
